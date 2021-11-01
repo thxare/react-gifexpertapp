492daae78656dc72d4554e5a090427a27d91ca14
thxare <thiareayleen@gmail.com>
2021-11-01 18:05:17 -0300

    GifGridItem actualizado

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const GifGridItem = ({id, title, url}) => {
-    console.log(id, title, url)
+    
     return (
         <div className='card animate__animated animate__bounce'>
             <img src= {url} alt={title}></img>
